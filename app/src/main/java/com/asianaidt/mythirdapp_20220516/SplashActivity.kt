ef8dcd8948de6dde191e71607294fc9222c6bcc0
uno <yoonjw@asianaidt.com>
2022-05-18 09:50:38 +0900

    splash 등록

--- app/src/main/java/com/asianaidt/mythirdapp_20220516/SplashActivity.kt
+++ app/src/main/java/com/asianaidt/mythirdapp_20220516/SplashActivity.kt
@@ -1,11 +1,21 @@
 package com.asianaidt.mythirdapp_20220516
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val myHandler = Handler(Looper.getMainLooper())
+        myHandler.postDelayed({
+            val myIntent = Intent(this, MainActivity::class.java)
+            startActivity(myIntent)
+            finish()
+                              }, 2000)
     }
 }
